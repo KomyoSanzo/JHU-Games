abe0ac3a0ebafef3688af7f42027766212e3c9ee
Alec Tabatchnick <Alec Tabatchnick>
2015-12-29 14:42:05 -0500

    Initial project commit

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+    // 1 - Designer variables
+
+    /// <summary>
+    /// Object speed
+    /// </summary>
+    public Vector2 speed = new Vector2(10, 10);
+
+    /// <summary>
+    /// Moving direction
+    /// </summary>
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+
+    private Rigidbody2D myRigidbody2D;
+
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 2 - Movement
+        movement = new Vector2(
+          speed.x * direction.x,
+          speed.y * direction.y);
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+        myRigidbody2D.velocity = movement;
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(5, 5);
+
+    // 2 - Store the movement
+    private Vector2 movement;
+
+    private Rigidbody2D myScriptsRigidbody2D;
+
+    void Start()
+    {
+        myScriptsRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+        // ...
+
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector2.up * 200 * Time.deltaTime, Space.World);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+        myScriptsRigidbody2D.velocity = movement;
+    }
+}
