a395bc1ab533dac297d04e9caadad3c974d3c7cc
Alec Tabatchnick <Alec Tabatchnick>
2016-01-03 01:12:36 -0500

    Fixed physic on the player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,57 +3,61 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-    /// <summary>
-    /// 1 - The speed of the ship
-    /// </summary>
-    public Vector2 speed = new Vector2(5, 5);
+    [HideInInspector]
+    public bool isFacingRight = true;
+    [HideInInspector]
+    public bool isJumping = false;
+    [HideInInspector]
+    public bool isGrounded = false;
 
-    // 2 - Store the movement
-    private Vector2 movement;
+    public float jumpForce = 650.0f;
+    public float maxSpeed = 7.0f;
 
-    private Rigidbody2D myScriptsRigidbody2D;
+    public Transform groundCheck;
+    public LayerMask groundLayers;
+
+    private float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D myRigidBody;
+
 
     void Start()
     {
-        myScriptsRigidbody2D = GetComponent<Rigidbody2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+
     }
 
     void Update()
     {
-        // 3 - Retrieve axis information
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
-        // 4 - Movement per direction
-        movement = new Vector2(
-          speed.x * inputX,
-          speed.y * inputY);
-        // ...
-
-        // 5 - Shooting
-        bool shoot = Input.GetButtonDown("Fire1");
-        shoot |= Input.GetButtonDown("Fire2");
-        // Careful: For Mac users, ctrl + arrow is a bad idea
-
-        if (shoot)
+        if(Input.GetButtonDown("Jump"))
         {
-            WeaponScript weapon = GetComponent<WeaponScript>();
-            if (weapon != null)
+            if(isGrounded == true)
             {
-                // false because the player is not an enemy
-                weapon.Attack(false);
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0);
+                myRigidBody.AddForce(new Vector2(0, jumpForce));
             }
         }
-        if (Input.GetKeyDown("space"))
-        {
-            transform.Translate(Vector2.up * 200 * Time.deltaTime, Space.World);
-        }
-
     }
 
     void FixedUpdate()
     {
-        // 5 - Move the game object
-        myScriptsRigidbody2D.velocity = movement;
+        isGrounded = Physics2D.OverlapCircle
+            (groundCheck.position, groundCheckRadius, groundLayers);
+
+        float move = Input.GetAxis("Horizontal");
+        myRigidBody.velocity = new Vector2(move * maxSpeed, myRigidBody.velocity.y);
+
+        if((move > 0.0f && isFacingRight == false) || (move <0.0f && isFacingRight == true))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 playerScale = transform.localScale;
+        playerScale.x = playerScale.x * -1;
+        transform.localScale = playerScale;
     }
 }
